d752c7e72824db6224f82e51a6a04c4f63178365
Mikołaj Ratajczyk <mikolaj.ratajczyk@gmail.com>
2022-07-11 19:05:19 +0200

    Make some fields of result DTO required

--- entities/result_dto.go
+++ entities/result_dto.go
@@ -1,8 +1,8 @@
 package entities
 
 type ResultDto struct {
-	Correct   int    `json:"correct"`
-	Total     int    `json:"total"`
-	TestId    string `json:"test_id"`
+	Correct   int    `json:"correct" binding:"required"`
+	Total     int    `json:"total" binding:"required"`
+	TestId    string `json:"test_id" binding:"required"`
 	CreatedAt int64  `json:"created_at"`
 }
