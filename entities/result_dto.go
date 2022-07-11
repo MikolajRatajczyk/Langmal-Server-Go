bed712025265196075339fafa8482258f58dfda4
Mikołaj Ratajczyk <mikolaj.ratajczyk@gmail.com>
2022-07-11 19:38:15 +0200

    Replace correct/total pair with correct/wrong in result

--- entities/result_dto.go
+++ entities/result_dto.go
@@ -2,7 +2,7 @@ package entities
 
 type ResultDto struct {
 	Correct   int    `json:"correct" binding:"required"`
-	Total     int    `json:"total" binding:"required"`
+	Wrong     int    `json:"wrong" binding:"required"`
 	TestId    string `json:"test_id" binding:"required"`
 	CreatedAt int64  `json:"created_at"`
 }
