b5145c85e2f54eb8fcc07c327b1612733cc23442
Mikołaj Ratajczyk <mikolaj.ratajczyk@gmail.com>
2023-02-25 21:46:41 +0100

    Fix parsing incorrect result data

--- controllers/results_controller.go
+++ controllers/results_controller.go
@@ -28,7 +28,7 @@ type resultsController struct {
 
 func (rc *resultsController) SaveResults(ctx *gin.Context) {
 	var resultDto models.ResultDto
-	err := ctx.ShouldBind(&resultDto)
+	err := ctx.BindJSON(&resultDto)
 	if err != nil {
 		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
 			"message": "Wrong result structure.",
--- models/result_dto.go
+++ models/result_dto.go
@@ -4,5 +4,5 @@ type ResultDto struct {
 	Correct   int    `json:"correct" binding:"number"`
 	Wrong     int    `json:"wrong" binding:"number"`
 	TestId    string `json:"test_id" binding:"required"`
-	CreatedAt int64  `json:"created_at"`
+	CreatedAt int64  `json:"created_at" binding:"required"`
 }
